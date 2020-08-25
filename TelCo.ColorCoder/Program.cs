1c73e3fdcc870e16fade2b32ad7bcc01b77c13d6
Gagan <gagan.punjabi@philips.com>
2020-08-25 16:03:16 +0530

    Removed read key

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -35,8 +35,6 @@ namespace TelCo.ColorCoder
             pairNumber = ColorCode.GetPairNumberFromColor(testPair2);
             Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}", testPair2, pairNumber);
             Debug.Assert(pairNumber == 6);
-            Console.ReadKey();
         }
-        
     }
 }
